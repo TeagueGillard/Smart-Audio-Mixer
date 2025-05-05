fc37c0a71ada93f60d5d481d6aac4bf12056f0dc
Teague Gillard <teaguegillard@gmail.com>
2025-05-05 20:35:40 +0100

    Add project files.

---
+++ Smart Audio Mixer Config/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Smart_Audio_Mixer_Config
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
